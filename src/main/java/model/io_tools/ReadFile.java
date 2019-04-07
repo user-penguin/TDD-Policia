30df48766b975b8e458236c3f96cf8971fc45c7d
user-penguin <kobzev96@gmail.com>
2019-04-08 02:11:56 +0700

    add read fromFile method

--- src/main/java/model/io_tools/ReadFile.java
+++ src/main/java/model/io_tools/ReadFile.java
@@ -1,8 +1,25 @@
 package model.io_tools;
 
+import java.io.*;
+
 public class ReadFile {
     // @TODO реализовать получение текста из файла
     public static String getText (String path) {
-        return "Я тестовый текст";
+        String pathToFile = ReadFile.class.getResource("/text/" + path).getPath();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(pathToFile);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+            String result = "";
+            String unit = "";
+            while((unit = bufferedReader.readLine()) != null) {
+                result += unit;
+            }
+            return result;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
