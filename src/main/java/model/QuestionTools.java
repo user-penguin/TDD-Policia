003252d4f3301c7a2a5b234d3a4bf6cc03f1c6b2
user-penguin <kobzev96@gmail.com>
2019-04-08 14:11:20 +0700

    realised method get answer by id

--- src/main/java/model/QuestionTools.java
+++ src/main/java/model/QuestionTools.java
@@ -1,8 +1,15 @@
 package model;
 
+import model.converter.JSONLib;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class QuestionTools {
-    // @TODO написать получение верного ответа на вопрос по индексу
     public static String getTrueAnswer(int i) {
-        return "майор";
+        JSONObject modules = JSONLib.getJsonFromFile("/json/answers_and_questions.json");
+        JSONArray questions = modules.getJSONArray("modules");
+        JSONArray concreteQuestion = questions.getJSONArray(2);
+        String result = concreteQuestion.getJSONObject(2).get("answer_true").toString();
+        return result;
     }
 }
