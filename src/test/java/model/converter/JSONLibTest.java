2116d143d2818a667c9708635a26ecd445c49930
user-penguin <kobzev96@gmail.com>
2019-04-08 12:43:54 +0700

    add array read test

--- src/test/java/model/converter/JSONLibTest.java
+++ src/test/java/model/converter/JSONLibTest.java
@@ -7,9 +7,22 @@ import static org.junit.Assert.assertEquals;
 
 public class JSONLibTest {
     @Test
-    public void fileToJson() {
+    public void fileToJson () {
         JSONObject expected = JSONLib.getJsonFromFile("test.json");
         JSONObject actual = new JSONObject("{\"kek\": 1}");
+        assertEquals(actual.toString(), expected.toString());
+    }
+
+    @Test
+    public void fileToJsonArrayTest () {
+        JSONObject expected = JSONLib.getJsonFromFile("arrayTest.json");
+        JSONObject actual = new JSONObject("{\n" +
+                " \"kek\":\n" +
+                "      [\n" +
+                "        {\"lol\":1},\n" +
+                "        {\"azzaza\": 2} \n" +
+                "      ]\n" +
+                "}");
         assertEquals(actual.toString(), expected.toString());
     }
 }
