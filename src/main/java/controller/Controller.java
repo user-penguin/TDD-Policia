5a23e6650c678a2d3a7928651672f75b02da8434
user-penguin <kobzev96@gmail.com>
2019-04-08 16:01:01 +0700

    add getting next question and prize size in form

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,4 +1,25 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import model.GameKernel;
+
+
 public class Controller {
+    @FXML
+    private TextField questionField, prizeField;
+
+    private GameKernel gameKernel;
+
+    @FXML
+    public void initialize() {
+        gameKernel = new GameKernel();
+    }
+
+    @FXML
+    public void getNextQuestion() {
+        questionField.setText(gameKernel.getNextQuestion());
+        prizeField.setText(String.valueOf(gameKernel.getPrizeSize()));
+    }
+
 }
--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         setMainStage(primaryStage);
-        Parent root = FXMLLoader.load(getClass().getResource("../../resources/fxml/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
